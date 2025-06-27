5ab0e160c172e7a3efc2b68262b1a37d46447a4a
Alan Betty <padayattilbettyalan@gmail.com>
2025-06-27 14:43:40 +0530

    login register system

---
+++ config-file.js
@@ -0,0 +1,8 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyC_99LRyPpiGKtpuY5kbarTOuWKxd4NoBg",
+    authDomain: "cosmic-ai-c7633.firebaseapp.com",
+    projectId: "cosmic-ai-c7633",
+    storageBucket: "cosmic-ai-c7633.firebasestorage.app",
+    messagingSenderId: "928977027464",
+    appId: "1:928977027464:web:1a2189da6ec0b99458ba3d"
+  };
