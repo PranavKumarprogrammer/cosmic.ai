206bbea0016f06f01bee21f68523c8f9f49137ee
Alan Betty <padayattilbettyalan@gmail.com>
2025-07-14 21:15:34 +0530

    Revert "change of time of logout"
    This reverts commit 995125526a6c43bc2a90e4e3c9b3791840e7e09f.

--- main.js
+++ main.js
@@ -154,14 +154,8 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log("Auth state changed:", user);
             currentUser = user;
             if (user) {
-                // Set user info for profile display
-                window.userDisplayName = user.displayName;
-                window.userPhotoURL = user.photoURL;
-                if (typeof renderUserProfile === 'function') renderUserProfile();
                 loadUserSearchHistory();
             } else {
-                window.userDisplayName = null;
-                window.userPhotoURL = null;
                 userSearchHistory = [];
                 window.userSearchHistory = userSearchHistory;
                 if (typeof renderSidebarHistory === "function") renderSidebarHistory();
@@ -614,8 +608,8 @@ function checkAutoLogout() {
     const loginTime = localStorage.getItem("cosmicai_login_time");
     if (loginTime) {
         const now = Date.now();
-        const fifteenMinutes = 15 * 60 * 1000;
-        if (now - parseInt(loginTime) > fifteenMinutes) {
+        const fourHours = 4 * 60 * 60 * 1000;
+        if (now - parseInt(loginTime) > fourHours) {
             alert("You have been automatically logged out due to inactivity. Please log in again.");
             doLogout(true);
         }
